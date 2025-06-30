3cf149cc89931eb1d91b97305b10505fbc5aed32
Ethan Stapley <ethan.t.stapley@gmail.com>
2025-06-29 18:18:40 -0600

    implemented ellipse

--- src/shapes/Ellipse.kt
+++ src/shapes/Ellipse.kt
@@ -1,10 +1,19 @@
 package shapes
 
+import kotlin.math.PI
+
+
 class Ellipse(
     val center: Point,
     val radii1: Double,
     val radii2: Double
 ) {
+    fun getArea(): Double {
+        return radii2 * radii1 * PI
+    }
 
+    fun move(x: Double, y: Double) {
+        center.movePoint(x, y)
+    }
 
 }
