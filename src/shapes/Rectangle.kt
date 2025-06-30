30ca51d4c748211c922eaf0bcf57abcf1fe01121
Ethan Stapley <ethan.t.stapley@gmail.com>
2025-06-29 18:09:06 -0600

    implemented the rectangle class

---
+++ src/shapes/Rectangle.kt
@@ -0,0 +1,27 @@
+package shapes
+
+import kotlin.math.abs
+
+class Rectangle(
+    private val point1: Point,
+    private val point2: Point
+) {
+
+    fun getHeight(): Double {
+        return abs(point2.y - point1.y)
+    }
+
+    fun getWidth(): Double {
+        return abs(point2.x - point1.x)
+    }
+
+    fun getArea(): Double {
+        return abs(getWidth() * getHeight())
+    }
+
+    fun move(x: Double, y: Double) {
+        point1.movePoint(x, y)
+        point2.movePoint(x, y)
+    }
+
+}
